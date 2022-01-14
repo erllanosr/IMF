280b57d826e55f8455414908a5d464b661d3cfb9
Erick Llanos <erick.llanos.clase@gmail.com>
2022-01-14 11:10:19 +0100

    programacion viernes 14

---
+++ Programación/Hoja2-2doTri/src/MetodosHoja2.java
@@ -0,0 +1,111 @@
+
+public class MetodosHoja2 {
+
+	public static void main(String[] args) {
+		
+	}
+	// Caso 1 - Pedir número.
+	private static int pedirNumero() {
+		
+		return 0;
+	}
+	
+	// Caso 1 - Mostrar por pantalla el número introducido en formato texto.
+	private static void mostrarNumeroTexto() {
+		
+	}
+	
+	// Caso 1 - Validar número si está entre 1-10.
+	private static void validarNumero() {
+		
+	}
+	
+	// Caso 1 - Mensaje de error si no está entre 1-10.
+	private static void mensajeError() {
+		
+	}
+	
+	// Caso 2 - Pedir un número entero.
+	private static int Numero () {
+		
+		return 0;
+	}
+	
+	// Caso 2 - Mostrar por pantalla el número introducido.
+	private static void mostrarNumTexto() {
+		
+	}
+	
+	// Caso 2 - Mensaje error si no está entre 1-10.
+	private static void mensajeErr() {
+		
+	}
+	
+	// Caso 3 - Pedir número del mes
+	private static byte numeroMes() {
+		
+		return 0;
+	}
+	
+	// Caso 3 - Devolver el número de días del mes dependiendo del mes.
+	private static void numeroDiasMes() {
+		
+	}
+	
+	// Caso 3 - Validar si el número está entre 1-12.
+	private static void validarNumMes() {
+		
+	}
+	
+	// Caso 3 - Mensaje error al validar.
+	private static void menError() {
+		
+	}
+	
+	// Caso 4 - pedir el número del dado.
+	private static byte numeroDado() {
+		
+		return 0;
+	}
+	
+	// Caso 4 - Mostrar por pantalla el número opuesto.
+	private static void numerOpuesto() {
+		
+	}
+	// Caso 4 - Validar si está entre el 1-6
+	private static void validarNumDado() {
+		
+	}
+	
+	// Caso 4 - Mensaje de error si no es del 1-6.
+	private static void mnsjeErr() {
+		
+	}
+	
+	// Caso 5
+	private static byte numeroHabitacion() {
+		
+		return 0;
+	}
+	
+	// Caso 6
+	private static byte valorNumero() {
+
+		return 0;
+	}
+	
+	// Caso 7
+	private static int numeroMayor(int numero) {
+		
+		return 0;
+	}
+	
+	// Caso 8 - Pedir precio artículo
+	private static byte precioArticulo() {
+		
+	return 0;
+	}
+	
+	// Caso 8 - 
+	
+}
